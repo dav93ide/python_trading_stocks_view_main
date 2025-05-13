5082742283322cd837c7a787497c1277744a2814
z3r0 <dav93ide@gmail.com>
2025-05-13 14:10:51 +0200

    commit

--- Environment.py
+++ Environment.py
@@ -1,16 +1,12 @@
 import os, logging, uuid
 from Classes.ConfigurationData import ConfigurationData
-from Classes.MainUser import MainUser
 from Resources import *
 from Utils.FileUtils import FileUtils
 from Utils.StoredDataUtils import StoredDataUtils
 
-from Mockups.Mockups import Mockups
-
 class Environment():
 
     __mConfigurationData: ConfigurationData = None
-    __mMainUser: MainUser = None
     __mLogger = None
 
     def __new__(cls):
@@ -22,44 +18,20 @@ class Environment():
     def get_configuration(self):
         return self.__mConfigurationData
 
-    def get_main_user(self):
-        return self.__mMainUser
-
     def get_logger(self):
         return self.__mLogger
 #endregion
 
-#region - Setter Methods
-    def set_main_user(self, mainUser):
-        self.__mMainUser = mainUser
-#endregion
-
 #region - Public Methods
-    def set_main_user(self, id, name, surname, username):
-        self.__mMainUser = MainUser()
-        self.__mMainUser.setId(id)
-        self.__mMainUser.setName(name)
-        self.__mMainUser.setSurname(surname)
-        self.__mMainUser.setUsername(username)
-
     def init(self):
         self.__init_directories()
         self.__init_configuration_data()
         self.__init_logger()
-        self.__init_main_user()
 #endregion
 
 #region - Private Methods
     def __init_configuration_data(self):
         self.__mConfigurationData = ConfigurationData.get_stored_data()
-
-    def __init_main_user(self):
-        self.__mMainUser = MainUser.get_stored_data()
-        if self.__mMainUser == None:
-            self.__mMainUser = MainUser(uuid.uuid4())
-            self.__mMainUser.set_tot_capital(Mockups.MOCKUP_USER_CAPITAL)
-            self.__mMainUser.store_data()
-        self.__mMainUser.set_from_stored_platform_data()
             
     def __init_logger(self):
         logging.basicConfig(level=logging.INFO)
@@ -74,5 +46,4 @@ class Environment():
         return  "####################\n"\
                 f"# {Environment.__name__}\n"\
                 f"{super().__str__()}\n"\
-                f"#- __mMainUser: {str(self.__mMainUser)}\n"\
                 "####################"
--- Frames/MainFrame.py
+++ Frames/MainFrame.py
@@ -2,11 +2,7 @@ import wx, os, uuid
 from Environment import Environment
 from Resources.Constants import *
 from Panels.MainPanel import MainPanel
-from Panels.PlatformDataPanel import PlatformDataPanel
-from Panels.NewTradingStrategyPanel import NewTradingStrategyPanel
-from Panels.NewBotPanel import NewBotPanel
 from Panels.ViewStocksPanel import ViewStocksPanel
-from Panels.AddStockApplicationData import AddStockApplicationData
 from Resources.Strings import Strings
 from Utils.RegexUtils import RegexUtils
 from Classes.ConfigurationData import ConfigurationData
@@ -15,11 +11,7 @@ from Networking.DataSynchronization import DataSynchronization
 class MainFrame(wx.Frame):
 
     __mMainPanel: MainPanel = None
-    __mPlatformDataPanel: PlatformDataPanel = None
-    __mNewTradingStrategyPanel: NewTradingStrategyPanel = None
-    __mNewBotPanel: NewBotPanel = None
     __mViewStocksPanel: ViewStocksPanel = None
-    __mAddStocksApplicationPanel: AddStockApplicationData = None
 
     __mMenubar: wx.MenuBar = None
 
@@ -46,65 +38,14 @@ class MainFrame(wx.Frame):
 #region - Init Menu Methods
     def __init_menubar(self):
         self.__mMenubar = wx.MenuBar()
-        self.__init_menu_main_menu()
-        self.__init_menu_settings()
-        self.__init_menu_views()
-        self.__init_menu_new_trading_strategy()
-        self.__init_menu_bot()
         self.__init_menu_stocks()
         self.SetMenuBar(self.__mMenubar)
 
-    def __init_menu_main_menu(self):
-        mainMenu = wx.Menu()
-        m00 = mainMenu.Append(-1, Strings.STR_MAIN_MENU)
-        self.__mMenubar.Append(mainMenu, Strings.STR_MAIN_MENU)
-        self.Bind(wx.EVT_MENU, self.__on_click_menu_main_menu, m00)
-
-    def __init_menu_settings(self):
-        settingsMenu = wx.Menu()
-        m11 = settingsMenu.Append(-1, Strings.STR_MENU_SETTINGS_PLATFORM_DATA)
-        m12 = settingsMenu.Append(-1, Strings.STR_MENU_SETTINGS_BOTS)
-        m13 = settingsMenu.Append(-1, Strings.STR_MENU_SETTINGS_SIMULATIONS)
-        self.__mMenubar.Append(settingsMenu, Strings.STR_MENU_SETTINGS)
-        self.Bind(wx.EVT_MENU, self.__on_click_menu_settings_platform_data, m11)
-        self.Bind(wx.EVT_MENU, self.__on_click_menu_settings_bots, m12)
-        self.Bind(wx.EVT_MENU, self.__on_click_menu_settings_simulations, m13)
-
-    def __init_menu_views(self):
-        viewMenu = wx.Menu()
-        m21 = viewMenu.Append(-1, Strings.STR_MENU_VIEW_ASSETS)
-        m22 = viewMenu.Append(-1, Strings.STR_MENU_VIEW_BOTS)
-        m23 = viewMenu.Append(-1, Strings.STR_MENU_VIEW_SIMULATIONS)
-        m24 = viewMenu.Append(-1, Strings.STR_MENU_VIEW_CHARTS)
-        self.__mMenubar.Append(viewMenu, Strings.STR_MENU_VIEW)
-        self.Bind(wx.EVT_MENU, self.__on_click_menu_view_assets, m21)
-        self.Bind(wx.EVT_MENU, self.__on_click_menu_view_bots, m22)
-        self.Bind(wx.EVT_MENU, self.__on_click_menu_view_simulations, m23)
-        self.Bind(wx.EVT_MENU, self.__on_click_menu_view_charts, m24)
-
-    def __init_menu_new_trading_strategy(self):
-        newTradingStrategyMenu = wx.Menu()
-        m31 = newTradingStrategyMenu.Append(-1, Strings.STR_MENU_TRADING_STRATEGY_NEW_TRADING_STRATEGY)
-        m32 = newTradingStrategyMenu.Append(-1, Strings.STR_MENU_TRADING_STRATEGY_NEW_DIVIDEND_STRATEGY)
-        m33 = newTradingStrategyMenu.Append(-1, Strings.STR_MENU_TRADING_STRATEGY_NEW_COPY_TRADER_STRATEGY)
-        self.__mMenubar.Append(newTradingStrategyMenu, Strings.STR_MENU_TRADING_STRATEGY)
-        self.Bind(wx.EVT_MENU, self.__on_click_menu_new_trading_strategy, m31)
-        self.Bind(wx.EVT_MENU, self.__on_click_menu_new_dividend_strategy, m32)
-        self.Bind(wx.EVT_MENU, self.__on_click_menu_new_copy_trader_strategy, m33)
-
-    def __init_menu_bot(self):
-        botMenu = wx.Menu()
-        m41 = botMenu.Append(-1, Strings.STR_MENU_BOT_NEW_BOT)
-        self.__mMenubar.Append(botMenu, Strings.STR_MENU_BOT)
-        self.Bind(wx.EVT_MENU, self.__on_click_menu_bot_new_bot, m41)
-
     def __init_menu_stocks(self):
         botMenu = wx.Menu()
-        m51 = botMenu.Append(-1, Strings.STR_MENU_STOCKS_VIEW)
-        m52 = botMenu.Append(-1, Strings.STR_MENU_ADD_STOCK_APPLICATION)
+        m11 = botMenu.Append(-1, Strings.STR_MENU_STOCKS_VIEW)
         self.__mMenubar.Append(botMenu, Strings.STR_MENU_STOCKS)
-        self.Bind(wx.EVT_MENU, self.__on_click_menu_stocks_view, m51)
-        self.Bind(wx.EVT_MENU, self.__on_click_menu_stocks_add_stock_application, m52)
+        self.Bind(wx.EVT_MENU, self.__on_click_menu_stocks_view, m11)
 #endregion
 
 #region - Init Panels Methods
@@ -113,109 +54,32 @@ class MainFrame(wx.Frame):
         self.__mMainPanel = MainPanel(self, wx.DisplaySize())
         self.__mMainPanel.Show()
 
-    def __init_platform_data_panel(self):
-        self.__remove_all_panels()
-        self.__mPlatformDataPanel = PlatformDataPanel(self, wx.DisplaySize())
-        self.__mPlatformDataPanel.Show()
-
-    def __init_new_trading_strategy_panel(self):
-        self.__remove_all_panels()
-        self.__mNewTradingStrategyPanel = NewTradingStrategyPanel(self, wx.DisplaySize())
-        self.__mNewTradingStrategyPanel.Show()
-
-    def __init_new_bot_panel(self):
-        self.__remove_all_panels()
-        self.__mNewBotPanel = NewBotPanel(self, wx.DisplaySize())
-        self.__mNewBotPanel.Show()
-
     def __init_view_stocks_panel(self):
         self.__remove_all_panels()
         self.__mViewStocksPanel = ViewStocksPanel(self, wx.DisplaySize(), None)
         self.__mViewStocksPanel.Show()
-
-    def __init_add_stocks_application_panel(self):
-        self.__remove_all_panels()
-        self.__mAddStocksApplicationPanel = AddStockApplicationData(self, wx.DisplaySize())
-        self.__mAddStocksApplicationPanel.Show()
-
 #endregion
 
 #region - Remove Panel Methods
     def __remove_all_panels(self):
         self.__remove_main_panel()
-        self.__remove_platform_data_panel()
-        self.__remove_new_trading_bot_panel()
-        self.__remove_new_bot_panel()
         self.__remove_view_stocks_panel()
-        self.__remove_add_stocks_application_panel()
 
     def __remove_main_panel(self):
         if self.__mMainPanel:
             self.__mMainPanel.Destroy()
 
-    def __remove_platform_data_panel(self):
-        if self.__mPlatformDataPanel:
-            self.__mPlatformDataPanel.Destroy()
-
-    def __remove_new_trading_bot_panel(self):
-        if self.__mNewTradingStrategyPanel:
-            self.__mNewTradingStrategyPanel.Destroy()
-
-    def __remove_new_bot_panel(self):
-        if self.__mNewBotPanel:
-            self.__mNewBotPanel.Destroy()
-
     def __remove_view_stocks_panel(self):
         if self.__mViewStocksPanel:
             self.__mViewStocksPanel.Destroy()
-
-    def __remove_add_stocks_application_panel(self):
-        if self.__mAddStocksApplicationPanel:
-            self.__mAddStocksApplicationPanel.Destroy()
 #endregion
 
 #region - On Click Methods
     def __on_click_menu_main_menu(self, evt):
         self.__init_main_panel()
 
-    def __on_click_menu_settings_platform_data(self, evt):
-        self.__init_platform_data_panel()
-
-    def __on_click_menu_settings_bots(self, evt):
-        logging.info("__on_click_menu_settings_bots")
-
-    def __on_click_menu_settings_simulations(self, evt):
-        logging.info("__on_click_menu_settings_simulations")
-
-    def __on_click_menu_view_assets(self, evt):
-        logging.info("__on_click_menu_view_assets")
-
-    def __on_click_menu_view_bots(self, evt):
-        logging.info("__on_click_menu_view_bots")
-
-    def __on_click_menu_view_simulations(self, evt):
-        logging.info("__on_click_menu_view_simulations")
-
-    def __on_click_menu_view_charts(self, evt):
-        logging.info("__on_click_menu_view_charts")
-
-    def __on_click_menu_new_trading_strategy(self, evt):
-        self.__init_new_trading_strategy_panel()
-
-    def __on_click_menu_new_dividend_strategy(self, evt):
-        logging.info("__on_click_menu_bon_click_menu_new_dividend_strategyot_new_dividend_bot")
-
-    def __on_click_menu_new_copy_trader_strategy(self, evt):
-        logging.info("__on_click_menu_new_copy_trader_strategy")
-
-    def __on_click_menu_bot_new_bot(self, evt):
-        self.__init_new_bot_panel()
-
     def __on_click_menu_stocks_view(self, evt):
         self.__init_view_stocks_panel()
-
-    def __on_click_menu_stocks_add_stock_application(self, evt):
-        self.__init_add_stocks_application_panel()
 #endregion
 
 #region - Check Configuration Methods
